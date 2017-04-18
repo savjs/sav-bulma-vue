724344f8d94459b4b6f4264b8450efffa47d4b28
ricky <ricky@hfjy.com>
2017-04-18 19:10:26 +0800

    radio文档

---
+++ docs/examples/Radio.vue
@@ -0,0 +1,12 @@
+<template>
+    <Radio v-model="single">Radio</Radio>
+</template>
+<script>
+    export default {
+        data () {
+            return {
+                single: false
+            }
+        }
+    }
+</script>
