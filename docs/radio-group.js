4f149e26f3b8cdb565e71892def437e16248d319
ricky <ricky@hfjy.com>
2017-04-20 16:53:04 +0800

    radioGroup组件文档

---
+++ docs/radio-group.js
@@ -0,0 +1,30 @@
+var radiogroup = {
+  props: [
+    {
+      name: 'value',
+      type: 'Boolean',
+      default: false,
+      description: `是否checked状态，可以使用 v-model 绑定数据`
+    },
+    {
+      name: 'name',
+      type: 'String',
+      default: '',
+      description: `相当于 input[type=radio] 元素的 name 属性`
+    }
+    {
+      name: 'options',
+      type: 'Array',
+      default: [],
+      description: `列表选项`
+    }
+  ],
+  examples: [
+    {
+      name: 'value',
+      file: 'examples/RadioGroup.vue'
+    }
+  ]
+}
+
+export {radiogroup}
