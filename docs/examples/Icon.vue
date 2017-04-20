dab2acee74d952adb012fb6c16beb4c5d3605722
wanghuihui <wanghuihui@hfjy.com>
2017-04-20 11:07:56 +0800

    icon

---
+++ docs/examples/Icon.vue
@@ -0,0 +1,22 @@
+<template>
+	<i :class="[theme.icon]" ></i>
+</template>
+
+<script>
+	export default {
+		props:{
+			icon: {
+				type: String,
+				default: '',
+			},
+			meta: {
+				type: String,
+				default: '',
+			},
+			theme: {
+				type: String,
+				default: ''
+			}
+		}
+	}
+</script>
