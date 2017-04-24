910bff948ce483e54601231f2a9dfc4fe5bf1ac0
Jython <luxiaofei@hfjy.com>
2017-04-24 22:02:02 +0800

    select.vue

---
+++ docs/examples/select.vue
@@ -0,0 +1,19 @@
+<template>
+  <sav-select :type="text" :placeholder="请选择" @on-change="ok" >
+  </sav-select>
+</template>
+<script>
+  export default {
+    data () {
+      return {
+        
+      }
+    },
+    methods: {
+            ok () {
+                alert("ok")
+            },
+            
+        }
+  }
+</script>
