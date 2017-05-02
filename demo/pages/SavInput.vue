ce910d8bbec15079b1a36f209db23c3a66e854d2
Jython <luxiaofei@hfjy.com>
2017-05-02 13:16:43 +0800

    modify input icon

--- demo/pages/SavInput.vue
+++ demo/pages/SavInput.vue
@@ -1,16 +1,16 @@
 <template>
   <div>
     <sav-input>
-      <span slot="leftIcon" class="icon is-small is-left" style="top:8px;">
+      <span slot="leftIcon" class="icon is-small is-left">
         <i class="fa fa-user"></i>
       </span>
-      <span slot="rightIcon" class="icon is-small is-right" style="top:8px;">
+      <span slot="rightIcon" class="icon is-small is-right">
         <i class="fa fa-check"></i>
       </span>
     </sav-input>
     <div style="width:200px;margin-bottom:15px;">
       <sav-input>
-      <span slot="leftIcon" class="icon is-small is-left" style="top:8px;">
+      <span slot="leftIcon" class="icon is-small is-left">
         <i class="fa fa-user"></i>
       </span>
     </sav-input>
