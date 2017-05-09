dd07a186e1af4dd6a7e5ff1afaacf2f4d9b57fe1
Jython <luxiaofei@hfjy.com>
2017-05-09 17:57:47 +0800

    add menu and tree docs

---
+++ docs/menu.js
@@ -0,0 +1,41 @@
+var menu = {
+    props: [
+        {
+            name: 'mode',
+            type: 'String',
+            default: 'vertical',
+            description: `菜单类型，可选值为 horizontal（水平） 和 vertical（垂直）`
+        },
+        {
+            name: 'active-name',
+            type: 'String | Number',
+            default: '',
+            description: `激活菜单的 name 值`
+        },
+        {
+            name: 'open-names',
+            type: 'Array',
+            default: [],
+            description: `展开的 Submenu 的 name 集合`
+        },
+        {
+            name: 'accordion',
+            type: 'Boolean',
+            default: false,
+            description: `是否开启手风琴模式，开启后每次至多展开一个子菜单`
+        }
+    ],
+    events: [
+        {
+            name: 'on-select',
+            description: `选择菜单（Menu-item）时触发`
+        },
+        {
+            name: 'on-open-change',
+            description: `当 展开/收起 子菜单时触发`
+        }
+    ],
+    examples: []
+}
+
+export {select}
---
+++ docs/tree.js
@@ -0,0 +1,49 @@
+var tree = {
+    props: [
+        {
+            name: 'data',
+            type: 'Array',
+            default: [],
+            description: `可嵌套的节点属性的数组，生成 tree 的数据`
+        },
+        {
+            name: 'multiple',
+            type: 'Boolean',
+            default: false,
+            description: `是否支持多选`
+        },
+        {
+            name: 'show-checkbox',
+            type: 'Boolean',
+            default: [],
+            description: `是否显示多选框`
+        }
+    ],
+    events: [
+        {
+            name: 'on-select-change',
+            description: `点击树节点时触发`
+        },
+        {
+            name: 'on-check-change',
+            description: `点击复选框时触发`
+        },
+        {
+            name: 'on-toggle-expand',
+            description: `展开和收起子列表时触发`
+        }
+    ],
+    methods: [
+        {
+            name: 'getCheckedNodes',
+            description: '获取被勾选的节点'
+        },
+        {
+            name: 'getSelectedNodes',
+            description: '获取被选中的节点'
+        }
+    ],
+    examples: []
+}
+
+export {select}
